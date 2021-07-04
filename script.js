1ee8ed6429a7c88d6d25591f382678d53e63d90e
Puma Prince <christopherbreen22@gmail.com>
2021-07-04 16:51:49 -0400

    added more html as well as tried to fetch the API that I added

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-var APIkey = "f43a73be09a020c8ebdcb66f12500bb5";
+var APIKey = "f43a73be09a020c8ebdcb66f12500bb5";
+var printedLocation = $('#LocationsList');
+var SearchBtn = $('.Search');
+
+var city;
+
+var weatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+console.log(weatherURL);
+
+
+fetch(weatherURL);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SearchBtn.on('click',function(event){
+//     event.preventDefault();
+//     console.log("");
+//     var userLocation = $(this).
+// })
